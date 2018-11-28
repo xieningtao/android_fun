5511d41519bb2d56f6314ba3c2f3ba43409f16fc
mac <1>
2018-11-28 23:51:04 +0800

    add tencent ad

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -83,3 +83,14 @@
 -keep class org.apache.http.**{*;}
 
 -keep com.baidu.platform.**{*;}
+
+#tencent ad
+-keep class com.qq.e.** {
+    public protected *;
+}
+-keep class android.support.v4.**{
+    public *;
+}
+-keep class android.support.v7.**{
+    public *;
+}
--- app/src/main/java/xnt/com/fun/SplashActivity.java
+++ app/src/main/java/xnt/com/fun/SplashActivity.java
@@ -1,10 +1,18 @@
 package xnt.com.fun;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.qq.e.ads.splash.SplashADListener;
+import com.qq.e.comm.util.AdError;
+import com.sf.loglib.L;
 import com.sflib.reflection.core.ThreadHelp;
 import com.tbruyelle.rxpermissions2.Permission;
 import com.tbruyelle.rxpermissions2.RxPermissions;
@@ -21,7 +29,7 @@ import xnt.com.fun.permission.NYPermissionUtil;
  * Created by mac on 2018/6/2.
  */
 
-public class SplashActivity extends FragmentActivity {
+public class SplashActivity extends FragmentActivity implements SplashADListener {
 
     private final String TAG = "SplashActivity";
 
@@ -36,6 +44,9 @@ public class SplashActivity extends FragmentActivity {
 //            public void onRequestSuccess() {
 //                L.info(TAG, "spot onRequestSuccess");
 //            }
+//        },2000);
+
+//        fetchSplashAD(this, container, skipView, Constants.APPID, Constants.SplashPosID, this, 0);
 //
 //            @Override
 //            public void onRequestFailed(int i) {
@@ -134,6 +145,11 @@ public class SplashActivity extends FragmentActivity {
     }
 
     @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         // 开屏展示界面的 onDestroy() 回调方法中调用
@@ -150,5 +166,80 @@ public class SplashActivity extends FragmentActivity {
     protected void onResume() {
         super.onResume();
         MobclickAgent.onResume(this);
+    }
+
+
+    /**
+     * 拉取开屏广告，开屏广告的构造方法有3种，详细说明请参考开发者文档。
+     *
+     * @param activity      展示广告的 activity
+     * @param adContainer   展示广告的大容器
+     * @param skipContainer 自定义的跳过按钮：传入该 view 给 SDK 后，SDK 会自动给它绑定点击跳过事件。SkipView 的样式可以由开发者自由定制，其尺寸限制请参考 activity_splash.xml 或下面的注意事项。
+     * @param appId         应用 ID
+     * @param posId         广告位 ID
+     * @param adListener    广告状态监听器
+     * @param fetchDelay    拉取广告的超时时长：即开屏广告从请求到展示所花的最大时长（并不是指广告曝光时长）取值范围[3000, 5000]，设为0表示使用广点通 SDK 默认的超时时长。
+     */
+    private void fetchSplashAD(Activity activity, ViewGroup adContainer, View skipContainer,
+                               String appId, String posId, SplashADListener adListener, int fetchDelay) {
+//        splashAD = new SplashAD(activity, adContainer, skipContainer, appId, posId, adListener, fetchDelay);
+    }
+
+    @Override
+    public void onADPresent() {
+        Log.i("AD_DEMO", "SplashADPresent");
+//        splashHolder.setVisibility(View.INVISIBLE); // 广告展示后一定要把预设的开屏图片隐藏起来
+    }
+
+    @Override
+    public void onADDismissed() {
+        L.info("AD_DEMO", "SplashADDismissed");
+        toHome();
+    }
+
+    private void toHome() {
+        Intent intent = new Intent(SplashActivity.this, NYHomeActivity.class);
+        SplashActivity.this.startActivity(intent);
+        SplashActivity.this.finish();
+    }
+
+
+    @Override
+    public void onNoAD(AdError error) {
+        L.info("AD_DEMO", String.format("LoadSplashADFail, eCode=%d, errorMsg=%s", error.getErrorCode(), error.getErrorMsg()));
+        toHome();
+    }
+
+    @Override
+    public void onADClicked() {
+        L.info("AD_DEMO", "SplashADClicked");
+    }
+
+    /**
+     * 倒计时回调，返回广告还将被展示的剩余时间。
+     * 通过这个接口，开发者可以自行决定是否显示倒计时提示，或者还剩几秒的时候显示倒计时
+     *
+     * @param millisUntilFinished 剩余毫秒数
+     */
+    @Override
+    public void onADTick(long millisUntilFinished) {
+        Log.i("AD_DEMO", "SplashADTick " + millisUntilFinished + "ms");
+//        skipView.setText(String.format(SKIP_TEXT,
+//                Math.round(millisUntilFinished / 1000f)));
+    }
+
+    @Override
+    public void onADExposure() {
+        Log.i("AD_DEMO", "SplashADExposure");
+    }
+
+    //防止用户返回键退出 APP
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                || keyCode == KeyEvent.KEYCODE_HOME) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
